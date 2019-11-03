a35f8823fe6cdf1dd50f61576cb72a4857d2567b
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2019-11-03 15:19:13 -0800

    Fix bug in updating goal pose from /cmd_pose info

--- scripts/pose_controller.py
+++ scripts/pose_controller.py
@@ -70,7 +70,7 @@ class PoseControllerNode:
         self.trans_listener = tf.TransformListener()
 
         ########## Code starts here ##########
-        # TODO: Create a subscriber to the '/cmd_pose' topic that receives
+        # DONE: Create a subscriber to the '/cmd_pose' topic that receives
         #       Pose2D messages and calls cmd_pose_callback.
         rospy.Subscriber('/cmd_pose', Pose2D, self.cmd_pose_callback)
 
@@ -91,10 +91,8 @@ class PoseControllerNode:
 
     def cmd_pose_callback(self, msg):
         ########## Code starts here ##########
-        # TODO: Update the goal pose in the pose controller.
-        self.x = msg.x
-        self.y = msg.y
-        self.theta = msg.theta
+        # DONE: Update the goal pose in the pose controller.
+        self.controller.load_goal(msg.x, msg.y, msg.theta)
         ########## Code ends here ##########
 
         # Record time of pose update
@@ -122,7 +120,7 @@ class PoseControllerNode:
                 pass
 
         ######### YOUR CODE HERE ############
-        # TODO: Use your pose controller to compute controls (V, om) given the
+        # DONE: Use your pose controller to compute controls (V, om) given the
         #       robot's current state.
         t = rospy.get_rostime().to_sec() - self.cmd_pose_time.to_sec();
         V, om = self.controller.compute_control(self.x, self.y, self.theta, t)
